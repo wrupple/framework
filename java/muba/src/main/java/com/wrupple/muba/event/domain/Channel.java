cc2f26252aa9d9f6566ec9b548a0c8d566c2f65f
japisan <japisan@wrupple.com>
2018-05-07 14:04:09 -0500

    Documented usage of channel domain

--- java/muba/src/main/java/com/wrupple/muba/event/domain/Channel.java
+++ java/muba/src/main/java/com/wrupple/muba/event/domain/Channel.java
@@ -2,6 +2,9 @@ package com.wrupple.muba.event.domain;
 
 import com.wrupple.muba.event.server.chain.RemoteServiceChain;
 
+/**
+ * Method to send events to a host
+ */
 public interface Channel extends CatalogEntry, RemoteServiceChain.Link{
 
     String CATALOG = "Channel";
--- java/muba/src/main/java/com/wrupple/muba/event/domain/ChannelAgreement.java
+++ java/muba/src/main/java/com/wrupple/muba/event/domain/ChannelAgreement.java
@@ -2,6 +2,9 @@ package com.wrupple.muba.event.domain;
 
 import com.wrupple.muba.event.domain.reserved.HasHostValue;
 
+/**
+ * Binds channel used to send events to a host
+ */
 public interface ChannelAgreement extends CatalogEntry,HasHostValue{
 
     String CATALOG = "ChannelAgreement";
