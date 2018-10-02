97c9294a51acf89b01ad179439dea969f26337f1
japisan <japisan@wrupple.com>
2018-10-01 20:48:42 -0500

    Short hand constructor for Event Resolvers

--- java/muba/src/main/java/com/wrupple/muba/event/server/service/impl/ImplicitEventResolverRegistration.java
+++ java/muba/src/main/java/com/wrupple/muba/event/server/service/impl/ImplicitEventResolverRegistration.java
@@ -1,10 +1,15 @@
 package com.wrupple.muba.event.server.service.impl;
 
+import com.wrupple.muba.event.domain.Contract;
+import com.wrupple.muba.event.domain.ContractDescriptor;
 import com.wrupple.muba.event.domain.ParentServiceManifest;
 import com.wrupple.muba.event.domain.ServiceManifest;
+import com.wrupple.muba.event.domain.impl.ServiceManifestImpl;
 import com.wrupple.muba.event.server.chain.command.RequestInterpret;
 import com.wrupple.muba.event.server.service.ImplicitEventResolver;
 import org.apache.commons.chain.Command;
+
+import java.util.Arrays;
 
 public abstract class ImplicitEventResolverRegistration implements ImplicitEventResolver.Registration {
 
@@ -15,6 +20,13 @@ public abstract class ImplicitEventResolverRegistration implements ImplicitEvent
 
     protected ImplicitEventResolverRegistration(ServiceManifest manifest, Command engine, RequestInterpret interpret, ParentServiceManifest parent) {
         this.manifest = manifest;
+        this.service = engine;
+        this.contractInterpret = interpret;
+        this.parent = parent;
+    }
+
+    protected ImplicitEventResolverRegistration(String distinguishedName, ContractDescriptor contract, Command engine, RequestInterpret interpret, ParentServiceManifest parent,String... grammar) {
+        this.manifest = new ServiceManifestImpl(distinguishedName, "1.0", contract, Arrays.asList(grammar));
         this.service = engine;
         this.contractInterpret = interpret;
         this.parent = parent;
