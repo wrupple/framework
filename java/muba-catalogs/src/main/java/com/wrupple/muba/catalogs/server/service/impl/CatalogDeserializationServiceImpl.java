1707f42e8f22edad445ecbd1fc9e13cdfb80e85d
japi <eljapi@gmail.com>
2019-06-21 12:19:33 -0500

    Java 11 compatibility

--- java/muba-catalogs/src/main/java/com/wrupple/muba/catalogs/server/service/impl/CatalogDeserializationServiceImpl.java
+++ java/muba-catalogs/src/main/java/com/wrupple/muba/catalogs/server/service/impl/CatalogDeserializationServiceImpl.java
@@ -4,11 +4,10 @@ import com.wrupple.muba.catalogs.domain.CatalogActionContext;
 import com.wrupple.muba.catalogs.server.service.CatalogDeserializationService;
 import com.wrupple.muba.event.domain.CatalogDescriptor;
 import com.wrupple.muba.event.domain.CatalogEntry;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class CatalogDeserializationServiceImpl implements CatalogDeserializationService {
     @Override
     public CatalogEntry deserialize(String rawSeed, CatalogDescriptor targetCatalog, CatalogActionContext context) throws Exception {
-        throw new NotImplementedException();//FIXME  object mapper
+        throw new RuntimeException("Not Implemented");//FIXME  object mapper
     }
 }
